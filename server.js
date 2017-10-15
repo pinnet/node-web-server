d9cf9c9b093635827d82d28f19fc9ca0227b0ab0
Danny Arnold <danny@dannyarnold.com>
2017-10-15 15:48:25 +0100

    end section

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+var maintenanceMode = false;
 
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine','hbs');
-app.use(express.static(__dirname + '/public'));
+
 
 app.use((req,res,next) =>{
 
     var now = new Date().toString();
+    var log = `${now}: ${req.method} : ${req.url} `;
+
+    fs.appendFile('server.log', log + '\n' , (err) =>{
+        if (err){   
+        console.log('Unable to append to server log' + err);
+        }
+    });
     next();
 });
-
+if (maintenanceMode) {
+    app.use((req,res,next) =>{
+        res.render('maintenance.hbs'); 
+    });  
+}
+app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
@@ -22,14 +36,12 @@ hbs.registerHelper('screamIt', (text) => {
 app.get('/', (req,res) => {
     res.render('home.hbs',{
         pageTitle: 'My Home Page',
-        currentYear: new Date().getFullYear(),
         welcomeMessage: 'Welcome to my web site'
     });
 });
 app.get('/about', (req,res)=> {
     res.render('about.hbs',{
         pageTitle: 'About',
-        currentYear: new Date().getFullYear()
     });
 })
 app.get('/bad', (req,res) => {
