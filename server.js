f1c34f8a455dc2a9f22fde5308ae7f663ba79777
Danny Arnold <danny@dannyarnold.com>
2017-10-15 14:49:11 +0100

    section complete

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require('express');
 const hbs = require('hbs');
 
 var app = express();
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine','hbs');
 app.use(express.static(__dirname + '/public'));
-
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
 app.get('/', (req,res) => {
     res.render('home.hbs',{
         pageTitle: 'My Home Page',
